aca1e37d78b13610705e4134442e01df3056e819
Jeffrey Alido <jalido@scc-pi2.scc.bu.edu>
2023-10-23 09:17:52 -0400

    removed the load config stuff, since it'll be passed into the constructor

--- sbrnet/model.py
+++ sbrnet/model.py
@@ -2,15 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from config_loader import load_config
-
 # types imports
 from torch import Tensor
 from torch.nn import Module, Conv2d, Sequential
 
-config = load_config("config.yaml")
+RSQRT2 = torch.sqrt(
+    0.5
+).item()  # NOTE: multiplication is faster than division for floats
 
-RSQRT2 = torch.sqrt(0.5).item() # NOTE: multiplication is faster than division for floats
 
 class SBRNet(Module):
     def __init__(self, config) -> None:
@@ -19,14 +18,14 @@ class SBRNet(Module):
         # UNet backbone is deprecated
         if config["backbone"] == "resnet":
             self.view_synthesis_branch: Module = ResNetCM2NetBlock()
-        
+
         self.init_convs()
-    
+
     def init_convs(self) -> None:
         def init_fn(mod: Module) -> None:
             if isinstance(mod, Conv2d):
                 nn.init.kaiming_normal_(mod.weight, nonlinearity="relu")
-        
+
         # initializes all Conv2d
         self.view_synthesis_branch.apply(init_fn)
 
@@ -39,7 +38,6 @@ class ResConnection(Sequential):
 # ResNet backbone
 class ResBlock(ResConnection):
     def __init__(self, channels: int = config["resnet_channels"]) -> None:
-
         super(ResBlock, self).__init__(
             nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(channels),
@@ -60,7 +58,11 @@ class ResNetCM2NetBlock(ResConnection):
         """
 
         super(ResNetCM2NetBlock, self).__init__(
-            nn.Conv2d(inchannels, outchannels, kernel_size=3, padding=1), # no activation or batch norm?
+            nn.Conv2d(
+                inchannels, outchannels, kernel_size=3, padding=1
+            ),  # no activation or batch norm?
             *(ResBlock() for i in range(numblocks)),
-            nn.Conv2d(outchannels, outchannels, kernel_size=3, padding=1) # no activation or batch norm?
+            nn.Conv2d(
+                outchannels, outchannels, kernel_size=3, padding=1
+            )  # no activation or batch norm?
         )
