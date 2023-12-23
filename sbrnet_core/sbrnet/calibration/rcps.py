0ad750b1d16abd07116f5d2e28ae3f7481db3e9b
Jeffrey Alido <jalido@bu.edu>
2023-12-23 15:01:31 -0500

    more on UQ calibration. not tested

--- sbrnet_core/sbrnet/calibration/rcps.py
+++ sbrnet_core/sbrnet/calibration/rcps.py
@@ -1,80 +1,156 @@
-from typing import List, Tuple
+from typing import Tuple
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.nn import Module
 
 
+def algorithm(
+    cal_data: Dataset,
+    model: Module,
+    alpha: float,
+    delta: float,
+    lamb_0: torch.Tensor,
+    d_lamb: float,
+    params: dict,
+) -> torch.Tensor:
+    lamb = lamb_0
+    UCB = torch.ones(len(lamb))
+
+    while torch.any(UCB > alpha):
+        lamb -= d_lamb * (UCB > alpha)
+        L = calc_all_L(cal_data=cal_data, model=model, lamb=lamb, params=params)
+        UCB = calc_UCB(L=L, delta=delta)
+    lamb = lamb + d_lamb
 
 
-
-def calc_UCB(L_list: torch.Tensor, delta: torch.float) -> torch.float:
-    """upper confidence bound by hoeffding
+def calc_UCB(L: torch.Tensor, delta: float) -> torch.Tensor:
+    """_summary_
 
     Args:
-        L_list (torch.Tensor): List of L_i's
-        delta (torch.float): number between 0 and 1.
+        L (torch.Tensor): size (num_batches, num_gt_layers)
+        delta (torch.float): float between 0 and 1
 
     Returns:
-        torch.float: _description_
+        torch.Tensor: size of num_gt_layers
     """
-    return torch.mean(L_list) + torch.sqrt(1/(2*len(L_list)) * torch.log(1/delta))
+    return torch.mean(L, dim=0) + torch.sqrt(
+        1 / (2 * L.shape[0]) * torch.log(1 / torch.tensor(delta))
+    )
 
 
 def calc_all_L(cal_data: Dataset, model, lamb, params: dict) -> torch.Tensor:
-    dataloader = DataLoader(cal_data, batch_size=16, shuffle=False, pin_memory=True)
-    L_list = torch.empty(len(dataloader), dtype=torch.float)
-    for i, (stack, rfv, gt) in enumerate(dataloader):
-        stack = stack.cuda()
-        rfv = rfv.cuda()
-        gt = gt.cuda()
-        with torch.no_grad():
-            L_list[i] = calc_one_L(
-                stack=stack, rfv=rfv, gt=gt, model=model, lamb=lamb, params=params
-            )
-    return L_list
-
-
-def calc_one_L(
-    stack: torch.Tensor, rfv: torch.Tensor, gt: torch.Tensor, model, lamb, params: dict
-) -> torch.float:
-    """L_i <- L(T_lamb(X_i))
+    """_summary_
 
     Args:
-        stack (torch.Tensor): _description_
-        rfv (torch.Tensor): _description_
-        gt (torch.Tensor): _description_
+        cal_data (Dataset): _description_
         model (_type_): _description_
         lamb (_type_): _description_
         params (dict): _description_
 
     Returns:
-        torch.float: _description_
+        torch.Tensor: size (num_batches, num_gt_layers)
+    """
+    dataloader = DataLoader(cal_data, batch_size=16, shuffle=False, pin_memory=True)
+    L_table = torch.empty(len(dataloader), params["num_gt_layers"])
+    for i, (stack, rfv, gt) in enumerate(dataloader):
+        stack = stack.cuda()
+        rfv = rfv.cuda()
+        gt = gt.cuda()
+        with torch.no_grad():
+            L_table[i, :] = calc_one_L(
+                stack=stack, rfv=rfv, gt=gt, model=model, lamb=lamb, params=params
+            )
+    return L_table
+
+
+def calc_one_L(
+    stack: torch.Tensor,
+    rfv: torch.Tensor,
+    gt: torch.Tensor,
+    model: Module,
+    lamb: torch.Tensor,
+    params: dict,
+) -> torch.Tensor:
+    """L_i <- L(T_lamb(X_i))
+
+    Args:
+        stack (torch.Tensor): B x num_views x H x W
+        rfv (torch.Tensor): B x num_rfv_layers x H x W
+        gt (torch.Tensor): B x num_gt_layers x H x W
+        model (Module): trained model
+        lamb (torch.Tensor): size is num_gt_layers
+        params (dict): config dict
+
+    Returns:
+        torch.Tensor: size of num_gt_layers
     """
     q_lo, q_hi, f = get_preds(model=model, stack=stack, rfv=rfv, params=params)
     low_T_lambda, high_T_lambda = form_T_lambda(f, q_lo, q_hi, lamb)
 
-    num_elements_between = torch.sum((gt > low_T_lambda) & (gt < high_T_lambda))
-    total_elements = gt.numel()
-    L = 1 - num_elements_between / total_elements
-    return L
+    num_gt_layers = gt.size(1)
+    L_values = []
+
+    for layer_idx in range(num_gt_layers):
+        gt_layer = gt[:, layer_idx, :, :]
+        low_T_lambda_layer = low_T_lambda[:, layer_idx, :, :]
+        high_T_lambda_layer = high_T_lambda[:, layer_idx, :, :]
+
+        num_elements_between = torch.sum(
+            (gt_layer >= low_T_lambda_layer) & (gt_layer <= high_T_lambda_layer)
+        )
+        total_elements = gt_layer.numel()
+        L = 1 - num_elements_between / total_elements
+        L_values.append(L)
+
+    return torch.stack(L_values)
 
 
-def form_T_lambda(f, q_lo, q_hi, lamb) -> Tuple(torch.Tensor, torch.Tensor):
-    low_T = f - lamb * (f - q_lo)
-    high_T = f + lamb * (q_hi - f)
+def form_T_lambda(
+    f: torch.Tensor, q_lo: torch.Tensor, q_hi: torch.Tensor, lamb: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Args:
+        f (torch.Tensor): B x num_gt_layers x H x W
+        q_lo (torch.Tensor): B x num_gt_layers x H x W
+        q_hi (torch.Tensor): B x num_gt_layers x H x W
+        lamb (torch.Tensor): num_gt_layers
+    Returns:
+        Tuple(torch.Tensor, torch.Tensor): each of shape (batch_size, num_gt_layers, H, W)
+    """
+
+    lamb_broadcasted = lamb.view(1, -1, 1, 1)  # 1 x num_gt_layers x 1 x 1
+    low_T = f - lamb_broadcasted * (f - q_lo)  # B x num_gt_layers x H x W
+    high_T = f + lamb_broadcasted * (q_hi - f)  # B x num_gt_layers x H x W
+
     return low_T, high_T
 
 
 def get_preds(
-    model, stack: torch.Tensor, rfv: torch.Tensor, params: dict
+    model: Module, stack: torch.Tensor, rfv: torch.Tensor, params: dict
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """_summary_
+
+    Args:
+        model (_type_): model
+        stack (torch.Tensor): B x num_views x H x W
+        rfv (torch.Tensor): B x num_rfv_layers x H x W
+        params (dict): config dict
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: each of shape (batch_size, num_gt_layers, H, W)
+    """
     slice_q_lo = slice(0, params["num_gt_layers"])
     slice_q_hi = slice(params["num_gt_layers"], params["num_gt_layers"] * 2)
     slice_point = slice(params["num_gt_layers"] * 2, None)
 
     with torch.no_grad():
         out = model(stack, rfv)
-        q_lo = (torch.sigmoid(out[:, slice_q_lo, :, :]),)
-        q_hi = (torch.sigmoid(out[:, slice_q_hi, :, :]),)
+        q_lo = torch.sigmoid(out[:, slice_q_lo, :, :])
+        q_hi = torch.sigmoid(out[:, slice_q_hi, :, :])
         f = out[:, slice_point, :, :]
     return q_lo, q_hi, f
+
+
+if __name__ == "__main__":
+    
+    lambdas = algorithm(cal_data=None, model=None, alpha=0.05, delta=0.05, lamb_0=None, d_lamb=0.1, params=None)
