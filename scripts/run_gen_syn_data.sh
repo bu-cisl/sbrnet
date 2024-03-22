b5e20c09d362e6b2f963fe5a742cb6e53e7a7c69
bonab2002 <rjbona@bu.edu>
2024-03-22 19:26:42 -0400

    fixed path

--- scripts/run_gen_syn_data.sh
+++ scripts/run_gen_syn_data.sh
@@ -2,5 +2,5 @@ timestamp=$(date +"%Y%m%d%H%M%S")
 jobname="sbrnet_synthetic_data_gen_${timestamp}"
 
 
-qsub -N "${jobname}" -o "/projectnb/tianlabdl/rjbona/sbrnet/.log/qsubs/${jobname}.qlog" /projectnb/tianlabdl/rjbonz/sbrnet/sbrnet_core/scripts/gen_syn_data.qsub
+qsub -N "${jobname}" -o "/projectnb/tianlabdl/rjbona/sbrnet/.log/qsubs/${jobname}.qlog" /projectnb/tianlabdl/rjbona/sbrnet/sbrnet_core/scripts/gen_syn_data.qsub
 
