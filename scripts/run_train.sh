56ac810dbc8119594b2adbcdee4ea97cd5b5bb3d
bonab2002 <rjbona@bu.edu>
2024-03-23 00:08:57 -0400

    added bce/pinball

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,4 +1,4 @@
 timestamp=$(date +"%Y%m%d%H%M%S")
 jobname="sbrnet_training_${timestamp}"
 
-qsub -N "${jobname}" -o "/projectnb/tianlabdl/rjbona/sbrnet/.log/qsubs/${jobname}.qlog" /projectnb/tianlabdl/rjbona/sbrnet/sbrnet_core/sbrnet/scripts/train.qsub
+qsub -N "${jobname}" -o "/projectnb/tianlabdl/rjbona/sbrnet/.log/qsubs/${jobname}.qlog" /projectnb/tianlabdl/rjbona/sbrnet/scripts/train.qsub
